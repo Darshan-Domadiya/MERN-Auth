04c8f0594ac13b9355e3b39e18f34d24035ee6be
Darshan-Domadiya <darshandomadiya3525@gmail.com>
2024-09-21 18:08:55 +0530

    Set up Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-success">
+      App
+      <h1 className="bg-danger">Hello MERN Auth</h1>
+    </div>
+  );
+};
+
+export default App;
