2ba6f441e4a648181d0610fe45db02c9cea12fc0
Darshan-Domadiya <darshandomadiya3525@gmail.com>
2024-10-25 11:21:54 +0530

    responsiveness of home and about page

--- frontend/src/pages/about/About.jsx
+++ frontend/src/pages/about/About.jsx
@@ -1,31 +1,37 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
 const About = () => {
   return (
-    <Container className="d-flex align-items-center justify-content-center flex-column w-50 mt-5">
-      <div className="text-black-50 mb-4">
-        <h2>About</h2>
-      </div>
+    <Container className="d-flex align-items-center justify-content-center flex-column mt-5">
+      <Row className="w-100">
+        <Col xs={12} md={8} lg={8} xl={6} className="mx-auto">
+          <div className="text-black-50 mb-4 text-center">
+            <h2>About</h2>
+          </div>
 
-      <div className="fw-medium">
-        <p>
-          This is a MERN (MongoDB, Express, React, Node.js) stack application
-          with authentication. It allows users to sign up, log in, and log out,
-          and provides access to protected routes only for authenticated users.
-        </p>
-        <p>
-          The front-end of the application is built with React and uses React
-          Router for client-side routing. The back-end is built with Node.js and
-          Express, and uses MongoDB as the database. Authentication is
-          implemented using JSON Web Tokens (JWT).
-        </p>
+          <div className="fw-medium">
+            <p>
+              This is a MERN (MongoDB, Express, React, Node.js) stack
+              application with authentication. It allows users to sign up, log
+              in, and log out, and provides access to protected routes only for
+              authenticated users.
+            </p>
+            <p>
+              The front-end of the application is built with React and uses
+              React Router for client-side routing. The back-end is built with
+              Node.js and Express, and uses MongoDB as the database.
+              Authentication is implemented using JSON Web Tokens (JWT).
+            </p>
 
-        <p>
-          This application is intended as a starting point for building
-          full-stack web applications with authentication using the MERN stack.
-        </p>
-      </div>
+            <p>
+              This application is intended as a starting point for building
+              full-stack web applications with authentication using the MERN
+              stack.
+            </p>
+          </div>
+        </Col>
+      </Row>
     </Container>
   );
 };
